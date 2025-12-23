3b1d748340f9aeaad1b05ef244a99a25c41fa129
Akash <akashkumarfzd35@gmail.com>
2025-12-23 14:24:12 +0530

    5.Static.py

---
+++ 5.Static.py
@@ -0,0 +1,18 @@
+# Define a static variable and access that through a class.
+
+class Python:   
+ staticVariable = 9 
+print(Python.staticVariable)
+
+#Change within an class
+Python.staticVariable = 12
+print(Python.staticVariable) 
+
+#Access through an instance
+instance = Python()
+print(instance.staticVariable) 
+
+#Change within an instance
+instance.staticVariable = 15
+print(instance.staticVariable) 
+print(Python.staticVariable)
